bb82cc89084d255ab64ef162e045aa4ba2a35d2c
eddy08 <bhardwajharsh08@gmail.com>
2022-08-02 00:19:54 +0530

    Search Bar is working fine 🔎

--- frontend/app/src/Pages/Search.js
+++ frontend/app/src/Pages/Search.js
@@ -1,4 +1,40 @@
+import { useEffect, useState } from "react";
+
 function Search() {
-  return <>Search for the Company</>;
+  const [input, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const onChangeHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+  let headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  useEffect(() => {
+    let raw = JSON.stringify({
+      search: input,
+      filter: "company",
+    });
+    let requestOptions = {
+      method: "POST",
+      headers: headers,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:4000/getCompaniesByName", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  });
+  return (
+    <>
+      <h1>Search for the Company</h1>
+      <input
+        type="text"
+        name="company"
+        value={input}
+        onChange={onChangeHandler}
+      />
+    </>
+  );
 }
 export default Search;
