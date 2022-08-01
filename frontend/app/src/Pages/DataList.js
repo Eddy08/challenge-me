78d5130266fbb09e4f5c38514bc87bf523a74227
eddy08 <bhardwajharsh08@gmail.com>
2022-08-02 01:37:44 +0530

    Search is working 30% 🔎

--- frontend/app/src/Pages/DataList.js
+++ frontend/app/src/Pages/DataList.js
@@ -1,6 +1,8 @@
-function DataList(){
-return (<>
-Companies List
-</>)
+function DataList() {
+  return (
+    <>
+      <h1>Companies List</h1>
+    </>
+  );
 }
 export default DataList;
--- frontend/app/src/Pages/Search.js
+++ frontend/app/src/Pages/Search.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 function Search() {
   const [input, setInputValue] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
+  const [suggestions, setSuggestions] = useState(["Please Enter Some Value"]);
   const onChangeHandler = (event) => {
     setInputValue(event.target.value);
   };
@@ -22,18 +22,28 @@ function Search() {
 
     fetch("http://localhost:4000/getCompaniesByName", requestOptions)
       .then((response) => response.text())
-      .then((result) => console.log(result))
+      .then((result) => {
+        console.log(result);
+        setSuggestions(result);
+      })
       .catch((error) => console.log("error", error));
-  });
+  },[input]);
   return (
     <>
       <h1>Search for the Company</h1>
       <input
         type="text"
         name="company"
+        list="companies"
         value={input}
         onChange={onChangeHandler}
       />
+      {/* <datalist id="companies"> */}
+     {suggestions}
+        {/* {suggestions.map((company, index) => (
+          <option key={index} value={company}></option>
+        ))} */}
+      {/* </datalist> */}
     </>
   );
 }
