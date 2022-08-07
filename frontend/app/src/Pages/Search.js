a90d3b209de077a9d7b6602ac678686f861d3d61
eddy08 <bhardwajharsh08@gmail.com>
2022-08-07 13:31:55 +0530

    Added MUI Support 🌫 and started  Autocomplete

--- frontend/app/src/Pages/Search.js
+++ frontend/app/src/Pages/Search.js
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react";
 import "./Search.css";
 function Search() {
+  const [companyDetails, setCompanyDetails] = useState({});
   const [errorValue, setErrorMessage] = useState("");
-  const [isSearching,setIsSearchingValue]=useState(false);
+  const [isSearching, setIsSearchingValue] = useState(false);
   const [input, setInputValue] = useState("");
   const [suggestions, setSuggestions] = useState([
     { company_name: "", company_id: "" },
   ]);
   const onChangeHandler = (event) => {
     setInputValue(event.target.value);
-    setIsSearchingValue(true)
+    setIsSearchingValue(true);
+  };
+  const addCompanyHandler = (event) => {
+    console.log("Inside addCompanyHandler");
+    let company = {};
+    company["name"] = event.target.value;
+    company["id"] = event.target.key;
+    setCompanyDetails(company);
   };
   let headers = new Headers();
   headers.append("Content-Type", "application/json");
@@ -30,21 +38,23 @@ function Search() {
       .then((result) => {
         // console.log("Data type of result",typeof(result))
         // console.log(result);
-        let resultValue=JSON.parse(result);
-        if(resultValue.length==0) {
-          setSuggestions([])
+        let resultValue = JSON.parse(result);
+        if (resultValue.length === 0) {
+          setSuggestions([]);
+          setErrorMessage("Company Not Present on Record 🧾");
+        } else {
+          setSuggestions(resultValue);
+          setErrorMessage("");
         }
-        else setSuggestions(resultValue);
-        setIsSearchingValue(false)
-        setErrorMessage("");
+        setIsSearchingValue(false);
       })
       .catch((error) => {
         setErrorMessage("Some Error Occured ❌");
-        setIsSearchingValue(false)
-        setSuggestions([])
-        console.log("error", error); 
+        setIsSearchingValue(false);
+        setSuggestions([]);
+        console.log("error", error);
       });
-  }, [input,errorValue,isSearching]);
+  }, [input, errorValue, isSearching]);
   return (
     <>
       <h1>Search for the Company</h1>
@@ -56,21 +66,32 @@ function Search() {
         value={input}
         onChange={onChangeHandler}
       />
+      {companyDetails["company_name"] !== "" ? (
+        <button type="submit">➕Add Company</button>
+      ) : (
+        "Some ERror"
+      )}
       {/* {isSearching  ? (<h4>Searching</h4>):""} */}
-      {
-        suggestions.length && !isSearching !== 0 ? (
-          <datalist id="companies">
+      {suggestions.length && !isSearching !== 0 ? (
+        <datalist id="companies" onChange={addCompanyHandler}>
           {suggestions.map((sug) => (
-            <option key={sug["company_id"]} value={sug["company_name"]}>{sug["company_name"]}</option>
+            <option
+              key={sug["company_id"]}
+              value={sug["company_name"]}
+              
+            >
+              {sug["company_name"]}
+            </option>
           ))}
-      </datalist>
-          
-        ) : (isSearching  ? (<h4>Searching</h4>):(<>
+        </datalist>
+      ) : isSearching ? (
+        <h4>Searching</h4>
+      ) : (
+        <>
           <datalist></datalist>
           <h4>No Data Found 😢</h4>
-          </>
-        ))
-        }
+        </>
+      )}
     </>
   );
 }
