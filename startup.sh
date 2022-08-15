a0ad29e3d1c249a923b18c7748ff0e5262fff80c
eddy08 <bhardwajharsh08@gmail.com>
2022-08-15 20:57:51 +0530

    Finally working 💚 updated with directory details

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm use node && npm run start && cd ../backend && nodemon index.js && /etc/init.d/postgresql restart 
+nvm use node && cd frontend/app && npm run start && cd ../backend && nodemon index.js && /etc/init.d/postgresql restart 
