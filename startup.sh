2936085b1a22aa8ea7431658ea39cd6680d405dd
eddy08 <bhardwajharsh08@gmail.com>
2022-08-15 21:10:43 +0530

    Redirecting the output to text files in sh script

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm use node && cd frontend/app && npm run start && cd ../backend && nodemon index.js && /etc/init.d/postgresql restart 
+nvm use node && /etc/init.d/postgresql restart  && cd frontend/app && npm run start > output1.txt && cd ../backend && nodemon index.js >output2.txt
