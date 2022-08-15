181ce6c34e4a6025788c28d58c5a3f0020cffb02
eddy08 <bhardwajharsh08@gmail.com>
2022-08-15 23:15:49 +0530

    Fixed the startup.sh file

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm use node && /etc/init.d/postgresql restart  && cd frontend/app && npm run start > output1.txt && cd ../backend && nodemon index.js >output2.txt
+# nvm use node && /etc/init.d/postgresql restart  && cd frontend/app && npm run build && cp -r build/ ../../backend/ui/ && cd ../../backend && nodemon index.js
+nvm use node && /etc/init.d/postgresql restart  && cd frontend/app && npm run start >output1.txt && cd ../../backend && nodemon index.js >output2.txt
