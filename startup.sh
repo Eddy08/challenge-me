5d8c54a0c97bbd5602105ae872de52826655c151
eddy08 <bhardwajharsh08@gmail.com>
2022-08-15 11:37:19 +0530

    modified startup.sh 💱

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /frontend/app &&  npm run start && cd ../backend && nodemon index.js && /etc/init.d/postgresql restart 
+npm run start && cd ../backend && nodemon index.js && /etc/init.d/postgresql restart 
