a65de971a0035cef9349d9af77b05159deffc909
eddy08 <bhardwajharsh08@gmail.com>
2022-08-15 20:43:09 +0530

    added Export Scripts for NVM

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-source /root/.bashrc && nvm use node && npm run start && cd ../backend && nodemon index.js && /etc/init.d/postgresql restart 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use node && npm run start && cd ../backend && nodemon index.js && /etc/init.d/postgresql restart 
